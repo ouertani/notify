ffe0028ac7ee2a3888c426d711d2d1605f073e83
Jonathan Ingram <jonathan.b.ingram@gmail.com>
2019-11-26 16:44:24 +1100

    sap

--- api/migrations/versions/0236_add_sap_sms_provider.py
+++ api/migrations/versions/0236_add_sap_sms_provider.py
@@ -17,7 +17,7 @@ down_revision = '0235'
 
 def upgrade():
     op.execute(
-        "INSERT INTO provider_details (id, display_name, identifier, priority, notification_type, active, supports_international, version) values ('{}', 'SAP', 'sap', 3, 'sms', true, true, 1)".format(str(uuid.uuid4()))
+        "INSERT INTO provider_details (id, display_name, identifier, priority, notification_type, active, supports_international, version) values ('{}', 'SAP', 'sap', 15, 'sms', true, true, 1)".format(str(uuid.uuid4()))
     )
 
     op.execute((
